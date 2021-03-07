5e6d218caac21189064fea2d379ba820d964fc9a
akliuxingyuan <akliuxingyuan@gmail.com>
2021-03-07 18:36:55 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Home } from './Components/Home'
+import { Department } from './Components/Department'
+import { Employee } from './Components/Employee'
+import { Navigation } from './Components/Navigation'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <p>hello world</p>
+      </div>
+      <Navigation />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/department' component={Department} />
+        <Route path='/employee' component={Employee} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
